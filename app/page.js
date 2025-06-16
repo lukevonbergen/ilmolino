ea7496cef6cdf53e182bf1d38840122cba987d9e
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-16 13:42:18 +0000

    AboutFix

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import TestimonialsIntermediate from "@/components/testimonials/Testimonials_2"
 import CTABasic from "@/components/cta/CTA_1"
 import Stats from "@/components/stats/Stats_2"
 import FindUsBasic from "@/components/findus/FindUs_1"
-import AboutIntermediate from "@/components/about/About_2"
+import About from "@/components/about/About_2"
 
 export default function HomePage() {
   return (
       <main>
         <HeroBasic />
-        <AboutBasic />
+        <About />
         <Stats />
         <FindUsBasic />
         <TestimonialsIntermediate />
